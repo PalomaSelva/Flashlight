967354c2e03c7cd0a7f4c6d91ee951c651188fa3
PalomaSelva <palomaselva1@gmail.com>
2022-06-11 10:35:53 -0300

    changes

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {View, SafeAreaView,StatusBar,Text,Image, StyleSheet} from 'react-native';
 const App = ()=>{
-  return  (
-    <SafeAreaView style={style.container_light_off}>  
-      <StatusBar backgroundColor={'black'}/>
+  const toggle = true;  
+return  (
+    <SafeAreaView style={ toggle ? style.container_light_on : style.container_light_off}>  
+      <StatusBar barStyle='dark-content' translucent={true} backgroundColor={'transparent'}/>
       <View>
-        
+        <Image style={ toggle ? style.img : style.img_Off} source={
+          toggle 
+          ? require('./assets/icons/eco-light.png') 
+          : require('./assets/icons/eco-light-off.png')
+        }/>
+
+        <Image style={ toggle ? style.img : style.img_Off} source={
+          toggle 
+          ? require('./assets/icons/logo-dio.png') 
+          : require('./assets/icons/logo-dio-white.png')
+        }/>
        
       </View>
     </SafeAreaView> 
@@ -24,7 +35,7 @@ const style = StyleSheet.create({
 container_light_on:{
     flex:1,
     alignItems:'center',
-    justifyContent:'center',
+    justifyContent: 'center',
     backgroundColor:'white',
 },
 
@@ -35,13 +46,20 @@ container_light_off:{
     backgroundColor:'black'
 },
 
-  texto:{
-    fontSize:70,
-    color:'white',
-    textShadowColor: 'rgba(0, 0, 0, 0.4)',
-    textShadowOffset: {width: 1, height: 2},
-    textShadowRadius: 5,
-    
 
-  },
+  img:{
+    resizeMode:'contain',
+    alignSelf:'center',
+    width:150,
+    height:150,
+},
+
+  img_Off:{
+    resizeMode:'contain',
+    alignSelf:'center',
+    tintColor:'white',
+    width:150,
+    height:150,
+},
+
 })
